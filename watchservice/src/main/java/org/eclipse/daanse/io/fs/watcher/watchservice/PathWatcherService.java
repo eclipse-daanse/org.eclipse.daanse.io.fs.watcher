8bb724cec7f5d200d9d051148beb99363dca0701
Stefan Bischof <stbischof@bipolis.org>
2024-06-27 16:12:55 +0200

    migrate code from org.eclipse.daanse.common repo
    Signed-off-by: Stefan Bischof <stbischof@bipolis.org>

---
+++ watchservice/src/main/java/org/eclipse/daanse/io/fs/watcher/watchservice/PathWatcherService.java
@@ -0,0 +1,111 @@
+/*
+* Copyright (c) 2024 Contributors to the Eclipse Foundation.
+*
+* This program and the accompanying materials are made
+* available under the terms of the Eclipse Public License 2.0
+* which is available at https://www.eclipse.org/legal/epl-2.0/
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Contributors:
+*   SmartCity Jena - initial
+*   Stefan Bischof (bipolis.org) - initial
+*/
+package org.eclipse.daanse.io.fs.watcher.watchservice;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.eclipse.daanse.io.fs.watcher.api.FileSystemWatcherListener;
+import org.osgi.service.component.ComponentServiceObjects;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.runtime.ServiceComponentRuntime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * {@link ReferenceCardinality#AT_LEAST_ONE} lets the PathWatcherService be
+ * activated only if it is really needed.
+ */
+@Component(service = {})
+public class PathWatcherService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PathWatcherService.class);
+
+    private final Map<ComponentServiceObjects<FileSystemWatcherListener>, FileSystemWatcherListener> listenersCSO = Collections
+            .synchronizedMap(new HashMap<>());
+
+    ExecutorService executorService;
+    private FileWatcherRunable fileWatcherRunable;
+
+    /**
+     * {@link Activate} Annotation on Constructor forces the
+     * {@link ServiceComponentRuntime} to create Instance before Binding a
+     * {@link FileSystemWatcherListener}
+     *
+     * @throws IOException
+     */
+    @Activate
+    public PathWatcherService() throws IOException {
+        LOGGER.info("constrcutor");
+
+        fileWatcherRunable = new FileWatcherRunable();
+        executorService = Executors.newVirtualThreadPerTaskExecutor();
+        executorService.execute(fileWatcherRunable);
+
+        LOGGER.info("activated");
+    }
+
+    @Deactivate
+    public void deActivate() {
+        LOGGER.info("deactivate - start");
+
+        fileWatcherRunable.shutdown();
+        executorService.close();
+
+        LOGGER.info("deactivate - end");
+
+    }
+
+    @Reference(cardinality = ReferenceCardinality.AT_LEAST_ONE, policy = ReferencePolicy.DYNAMIC)
+    void bindFileSystemWatcherListener(ComponentServiceObjects<FileSystemWatcherListener> listenerCSO,
+            Map<String, Object> map) throws IOException, InterruptedException {
+
+        LOGGER.info("bind FileSystemWatcherListener with properties: {}", map);
+
+        FileSystemWatcherListener listener = listenerCSO.getService();
+        listenersCSO.put(listenerCSO, listener);
+
+        if (listener == null) {
+            LOGGER.warn("Could not get FileSystemWatcherListener-Service of: {}, props: {}", listenerCSO, map);
+            return;
+        }
+
+        fileWatcherRunable.addFileWatcherRunable(listener, map);
+
+    }
+
+    void unbindFileSystemWatcherListener(ComponentServiceObjects<FileSystemWatcherListener> listenerCSO,
+            Map<String, Object> map) {
+        FileSystemWatcherListener listener = listenersCSO.remove(listenerCSO);
+
+        LOGGER.info("unbind FileSystemWatcherListener with properties: {}", map);
+
+        if (listener == null) {
+            LOGGER.warn("Service not handled: {}", listenerCSO);
+            return;
+        }
+        listenerCSO.ungetService(listener);
+        fileWatcherRunable.removeFileWatcherRunable(listener);
+    }
+
+}
---
+++ watchservice/src/test/java/org/eclipse/daanse/io/fs/watcher/watchservice/OSGiServiceTest.java
@@ -0,0 +1,113 @@
+/*
+* Copyright (c) 2024 Contributors to the Eclipse Foundation.
+*
+* This program and the accompanying materials are made
+* available under the terms of the Eclipse Public License 2.0
+* which is available at https://www.eclipse.org/legal/epl-2.0/
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Contributors:
+*   SmartCity Jena - initial
+*   Stefan Bischof (bipolis.org) - initial
+*/
+package org.eclipse.daanse.io.fs.watcher.watchservice;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+import static org.osgi.test.common.dictionary.Dictionaries.asDictionary;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardWatchEventKinds;
+import java.time.Duration;
+import java.util.Map;
+
+import org.eclipse.daanse.io.fs.watcher.api.FileSystemWatcherListener;
+import org.eclipse.daanse.io.fs.watcher.api.FileSystemWatcherWhiteboardConstants;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.io.TempDir;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.cm.annotations.RequireConfigurationAdmin;
+import org.osgi.service.component.annotations.RequireServiceComponentRuntime;
+import org.osgi.test.common.annotation.InjectBundleContext;
+import org.osgi.test.junit5.context.BundleContextExtension;
+
+import aQute.bnd.annotation.spi.ServiceProvider;
+
+@RequireServiceComponentRuntime
+@RequireConfigurationAdmin
+@ExtendWith(BundleContextExtension.class)
+@ServiceProvider(value = FileSystemWatcherListener.class)
+class OSGiServiceTest {
+
+    private static Duration WAIT_MOST = Duration.ofSeconds(10);
+    @InjectBundleContext
+    BundleContext bc;
+
+    @TempDir
+    Path path;
+
+    @RepeatedTest(value = 200)
+    void testFileSystemListener() throws Exception {
+
+        StoringFileSystemWatcherListener listener = new StoringFileSystemWatcherListener();
+
+        Path file_preexist = Files.createTempFile(path, "pre_exist1", ".txt");
+        Files.writeString(file_preexist, "1");
+
+        Map<String, Object> map = Map.of(FileSystemWatcherWhiteboardConstants.FILESYSTEM_WATCHER_PATH,
+                path.toAbsolutePath().toString(), FileSystemWatcherWhiteboardConstants.FILESYSTEM_WATCHER_RECURSIVE,
+                "true");
+
+        ServiceRegistration<FileSystemWatcherListener> sreg = bc.registerService(FileSystemWatcherListener.class,
+                listener, asDictionary(map));
+
+        await().atMost(WAIT_MOST).until(() -> listener.getInitialPaths().size() == 1);
+
+        assertThat(listener.getInitialPaths()).hasSize(1);
+        assertThat(listener.getInitialPaths().poll()).isEqualTo(file_preexist);
+
+        Path file_created = Files.createTempFile(path, "created1", ".txt");// create
+        Files.delete(file_preexist);// delete
+        Files.writeString(file_created, "2");// modify
+
+        await().atMost(WAIT_MOST).until(() -> listener.getEvents().size() >= 3);// sometime multiple modify
+
+        assertThat(listener.getEvents()).hasSizeGreaterThanOrEqualTo(3);
+        assertThat(listener.getEvents().peek().getKey()).isEqualTo(file_created);
+        assertThat(listener.getEvents().poll().getValue()).isEqualTo(StandardWatchEventKinds.ENTRY_CREATE);
+
+        assertThat(listener.getEvents().peek().getKey()).isEqualTo(file_preexist);
+        assertThat(listener.getEvents().poll().getValue()).isEqualTo(StandardWatchEventKinds.ENTRY_DELETE);
+
+        assertThat(listener.getEvents().peek().getKey()).isEqualTo(file_created);
+        assertThat(listener.getEvents().poll().getValue()).isEqualTo(StandardWatchEventKinds.ENTRY_MODIFY);
+
+        // maybe more modify existing, then clear
+        listener.getEvents().clear();
+
+        Path dir1 = Files.createDirectory(path.resolve("dir1"));// create dir
+        await().atMost(WAIT_MOST).pollDelay(Duration.ofMillis(100)).pollInterval(Duration.ofMillis(100))
+                .until(() -> listener.getEvents().size() == 1);
+
+        Path f1InDir1 = Files.createTempFile(dir1, "af1", ".txt");// create
+
+        await().atMost(WAIT_MOST).until(() -> listener.getEvents().size() == 2);
+
+        assertThat(listener.getEvents()).hasSize(2);
+        assertThat(listener.getEvents().peek().getKey()).isEqualTo(dir1);
+        assertThat(listener.getEvents().poll().getValue()).isEqualTo(StandardWatchEventKinds.ENTRY_CREATE);
+
+        assertThat(listener.getEvents().peek().getKey()).isEqualTo(f1InDir1);
+        assertThat(listener.getEvents().poll().getValue()).isEqualTo(StandardWatchEventKinds.ENTRY_CREATE);
+
+        sreg.unregister();
+        Files.createTempFile(dir1, "wf2", ".txt");// create
+        await().pollDelay(Duration.ofMillis(200)).atMost(WAIT_MOST).until(() -> listener.getEvents().isEmpty());
+
+    }
+
+}
